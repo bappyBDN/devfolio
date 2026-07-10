a019582439ade97877ce92ae067279c4ee18b25b
Bappy Chandra Debnath <142099534+bappyBDN@users.noreply.github.com>
2026-07-10 18:48:36 +0600

    Add WHITENOISE_MANIFEST_STRICT setting to settings.py

--- core/settings.py
+++ core/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
+WHITENOISE_MANIFEST_STRICT = False
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
